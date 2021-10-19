89e642f284168c64f53a5b10459fa41d7601dfe0
Kevin <kevin.liao@ambernet.com.tw>
2021-10-19 17:20:52 +0800

    修改swaggerconfig

--- src/main/java/com/example/demoswagger/controller/BookController.java
+++ src/main/java/com/example/demoswagger/controller/BookController.java
@@ -12,9 +12,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Objects;
 
-@Api(tags = "Book")
+@Api(tags = "Book API")
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/api/v1/book")
 public class BookController {
 
     @Autowired
@@ -22,7 +22,7 @@ public class BookController {
 
     @ApiOperation(value = "取得書本", notes = "列出所有書本")
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping(value = "/v1/book/getAllBooks", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @GetMapping(value = "/getAllBooks", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public List<Book> getAll() {
         return bookRepository.findAll();
     }
@@ -30,7 +30,7 @@ public class BookController {
     @ApiOperation(value = "新增書本", notes = "新增書本的內容")
     @ApiResponses(value = {@ApiResponse(code = 201, message = "存檔成功")})
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping(value = "/v1/book/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public BookDto create(
             @ApiParam(required = true, value = "書本內容") @RequestBody BookDto bookDto) {
         Book book = new Book();
@@ -45,7 +45,7 @@ public class BookController {
     @ApiOperation(value = "取得書本內容", notes = "取得書本內容")
     @ApiResponses(value = {@ApiResponse(code = 200, message = "書本資訊")})
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping(value = "/v1/book/{bookId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{bookId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public BookDto get(
             @ApiParam(required = true, name = "bookId", value = "書本ID") @PathVariable Integer bookId) {
         Book book = bookRepository.findByBookId(bookId);
@@ -61,7 +61,7 @@ public class BookController {
     @ApiOperation(value = "刪除書本", notes = "刪除書本的內容")
     @ApiResponses(value = {@ApiResponse(code = 200, message = "刪除成功")})
     @ResponseStatus(HttpStatus.OK)
-    @DeleteMapping(value = "/v1/book/delete/{bookId}")
+    @DeleteMapping(value = "/delete/{bookId}")
     public void delete(
             @ApiParam(required = true, name = "bookId", value = "書本ID") @PathVariable Integer bookId) {
         Book book = bookRepository.findByBookId(bookId);
